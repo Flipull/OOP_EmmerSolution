b48388b7a9faf329c2f303410f485808ca6c7442
Mark Jansen <markwilemail@gmail.com>
2020-11-26 11:51:36 +0100

    Changed Enum to reflect useful values

--- LiquidContainerLibrary/Regenton.cs
+++ LiquidContainerLibrary/Regenton.cs
@@ -6,28 +6,14 @@ namespace LiquidContainerLibrary
 {
     public class Regenton: LiquidContainerAbstract
     {
-        public enum Size { Small, Medium, Large};
+        public enum Size { Small = 80, Medium = 120, Large = 160};
         
         public Regenton(uint content = 0, Regenton.Size size = Size.Small)
         {
-            switch (size)
-            {
-                case Size.Small:
-                    Capacity = 80;
-                    break;
-                case Size.Medium:
-                    Capacity = 120;
-                    break;
-                case Size.Large:
-                    Capacity = 160;
-                    break;
-                default:
-                    throw new ArgumentException();
-            }
-
+            Capacity = (uint)size;
+            
             if (content < 0 || content > Capacity)
                 throw new ArgumentException();
-
             Fill(content);
         }
     }
